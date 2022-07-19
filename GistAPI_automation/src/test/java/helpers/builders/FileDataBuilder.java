bcfa4408c34b21904d09e2e623137406927288c0
pooja mehta <poojamehta1197@gmail.com>
2022-07-19 07:02:27 +0530

    Add method createFile and withContent

--- GistAPI_automation/src/test/java/helpers/builders/FileDataBuilder.java
+++ GistAPI_automation/src/test/java/helpers/builders/FileDataBuilder.java
@@ -1,5 +1,29 @@
 package helpers.builders;
 
-public class FileDataBuilder {
+import models.File;
+import models.FileProperties;
 
+public class FileDataBuilder {
+    private File file;
+    private FileProperties fileProperties;
+
+    public FileDataBuilder() {
+        file = new File();
+        fileProperties = new FileProperties();
+        file.setFileProperties(fileProperties);
+    }
+
+    public FileDataBuilder createFile(String fileName) {
+        file.setName(fileName);
+        return this;
+    }
+
+    public FileDataBuilder WithContent(String content) {
+        fileProperties.setContent(content);
+        return this;
+    }
+
+    public File build() {
+        return file;
+    }
 }
