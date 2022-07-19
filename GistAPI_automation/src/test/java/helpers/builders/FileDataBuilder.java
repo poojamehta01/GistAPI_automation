f88b7cd71a731b52aa7c1a5536399f37d1bdf8f9
pooja mehta <poojamehta1197@gmail.com>
2022-07-19 06:59:29 +0530

    Revert "Add desc/public to build file"
    This reverts commit f14519c0fb817243922fdff6cc5d9001132c43af.

--- GistAPI_automation/src/test/java/helpers/builders/FileDataBuilder.java
+++ GistAPI_automation/src/test/java/helpers/builders/FileDataBuilder.java
@@ -1,29 +1,5 @@
 package helpers.builders;
 
-import models.File;
-import models.FileProperties;
-
 public class FileDataBuilder {
-    private File file;
-    private FileProperties fileProperties;
 
-    public FileDataBuilder() {
-        file = new File();
-        fileProperties = new FileProperties();
-        file.setFileProperties(fileProperties);
-    }
-
-    public FileDataBuilder createFile(String fileName) {
-        file.setName(fileName);
-        return this;
-    }
-
-    public FileDataBuilder WithContent(String content) {
-        fileProperties.setContent(content);
-        return this;
-    }
-
-    public File build() {
-        return file;
-    }
 }
